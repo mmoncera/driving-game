d6d791e9c9dce2cbf184ed293d55c2333ffb62c7
Michael Moncera <michaelmoncera@gmail.com>
2022-02-01 20:10:17 -0800

    Added a boolean to your data model to indicate whether or not the car is started. #4

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
 var $car = document.querySelector('.car');
-var carDirection = {
+var carInfo = {
   currentDirection: 'east',
-  ArrowRight: 'east',
-  ArrowDown: 'south',
-  ArrowLeft: 'west',
-  ArrowUp: 'north',
-  x: $car.getBoundingClientRect().left,
-  y: $car.getBoundingClientRect().top
+  directions: {
+    ArrowRight: 'east',
+    ArrowDown: 'south',
+    ArrowLeft: 'west',
+    ArrowUp: 'north'
+  },
+  coordinates: {
+    x: $car.getBoundingClientRect().left,
+    y: $car.getBoundingClientRect().top
+  },
+  isStarted: false
 };
 
 function startCar() {
-  carDirection.x += 10;
-  $car.style.left = carDirection.x + 'px';
+  carInfo.coordinates.x += 10;
+  $car.style.left = carInfo.coordinates.x + 'px';
 }
 
 function handleTurn(event) {
   var key = event.key;
-  if (key in carDirection === false) {
+  if (carInfo.isStarted) {
     return;
   }
-  $car.classList.replace(carDirection.currentDirection, carDirection[key]);
-  carDirection.currentDirection = carDirection[key];
+  $car.classList.replace(carInfo.currentDirection, carInfo.directions[key]);
+  carInfo.currentDirection = carInfo.directions[key];
 }
 
 function handleMovement(event) {
   var key = event.key;
   if (key === ' ') {
     setInterval(startCar, 16);
+    carInfo.isStarted = true;
   }
 }
 
